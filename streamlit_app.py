ea54788b1539b243ef0761a5711d36c891b75532
Dominick Miceli <dcmiceli@syr.edu>
2026-09-13 21:02:46 -0400

    spelling correction

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.title("HW Manager")
 hw1 = st.Page('HW/HW1.py', title='HW1')
 hw2 = st.Page('HW/HW2.py', title='HW2')
-hw3 = st.Page('HW/HW3.py', title='HW3', defaul=True)
+hw3 = st.Page('HW/HW3.py', title='HW3', default=True)
 
 pg = st.navigation([hw1, hw2, hw3])
 pg.run()
