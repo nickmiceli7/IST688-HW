18e7be3d0b4b349b50aabb191a724f08274d8328
Dominick Miceli <dcmiceli@syr.edu>
2026-09-13 21:01:37 -0400

    debug

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,8 @@ import streamlit as st
 
 st.title("HW Manager")
 hw1 = st.Page('HW/HW1.py', title='HW1')
-hw2 = st.Page('HW/HW2.py', title='HW2', default=True)
+hw2 = st.Page('HW/HW2.py', title='HW2')
+hw3 = st.Page('HW/HW3.py', title='HW3', defaul=True)
 
-pg = st.navigation([hw1, hw2])
+pg = st.navigation([hw1, hw2, hw3])
 pg.run()
